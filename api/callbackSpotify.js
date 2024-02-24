371c24301a77b3ded33a23aa6b9b278ab029f02f
Charudatta Potdar <charupot08@gmail.com>
2024-02-24 14:59:05 -0700

    backend on ec2

--- api/callbackSpotify.js
+++ api/callbackSpotify.js
@@ -1,4 +1,4 @@
 module.exports = (req, res) => {
   const code = req.query.code;
-  res.redirect(`http://localhost:3000/auth/spotify/callback?code=${code}`);
+  res.redirect(`http://3.218.63.70:3000/auth/spotify/callback?code=${code}`);
 };
