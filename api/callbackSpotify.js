51158f8b4342684253c05a9d0a60c184eef5e1a0
Charudatta Potdar <charupot08@gmail.com>
2024-02-24 14:47:59 -0700

    added call back spotify - now working

--- api/callbackSpotify.js
+++ api/callbackSpotify.js
@@ -1,5 +1,4 @@
 module.exports = (req, res) => {
-    const code = req.query.code;
-    res.send(code);
-  };
-  
+  const code = req.query.code;
+  res.redirect(`http://localhost:3000/auth/spotify/callback?code=${code}`);
+};
