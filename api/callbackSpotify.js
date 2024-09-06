fa03c1367c3d95a5996b563214cd9a9e2cb05f25
Charudatta Potdar <charupot08@gmail.com>
2024-09-06 15:10:10 -0700

    IP address corrected

--- api/callbackSpotify.js
+++ api/callbackSpotify.js
@@ -1,4 +1,4 @@
 module.exports = (req, res) => {
   const code = req.query.code;
-  res.redirect(`http://172-31-36-12:3000/auth/spotify/callback?code=${code}`);
+  res.redirect(`http://172.31.36.12:3000/auth/spotify/callback?code=${code}`);
 };
